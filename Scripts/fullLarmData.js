a60614011d89547afaf857e1512037e7f735199d
paladinlee <paladinlee@gmail.com>
2019-06-27 19:12:32 +0800

    更新全廣130

--- Scripts/fullLarmData.js
+++ Scripts/fullLarmData.js
@@ -1,4 +1,5 @@
 var arrayDailyLamID = [
+'130',
 '129',
 '128',    
 '127',
@@ -129,6 +130,7 @@ var arrayDailyLamID = [
 '2',
 '1'];
         var arrayDailyLamFile = [
+'https://d3v2d3f6zmz0pr.cloudfront.net/mp3/lr2/%E7%A6%8F%E6%99%BA%E5%85%A8%E7%90%83%E5%BB%A3%E8%AB%96%E7%AC%AC%E4%BA%8C%E8%BC%AA_0130.mp3',
 'https://d3v2d3f6zmz0pr.cloudfront.net/mp3/lr2/%E7%A6%8F%E6%99%BA%E5%85%A8%E7%90%83%E5%BB%A3%E8%AB%96%E7%AC%AC%E4%BA%8C%E8%BC%AA_0129.mp3',
 'https://d3v2d3f6zmz0pr.cloudfront.net/mp3/lr2/%E7%A6%8F%E6%99%BA%E5%85%A8%E7%90%83%E5%BB%A3%E8%AB%96%E7%AC%AC%E4%BA%8C%E8%BC%AA_0128.mp3',            
 'https://d3v2d3f6zmz0pr.cloudfront.net/mp3/lr2/%E7%A6%8F%E6%99%BA%E5%85%A8%E7%90%83%E5%BB%A3%E8%AB%96%E7%AC%AC%E4%BA%8C%E8%BC%AA_0127.mp3',
