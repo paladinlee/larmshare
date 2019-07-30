3d1150d50ef66f205c915e346ddd7bcb097194bb
paladinlee <paladinlee@gmail.com>
2019-07-30 14:00:02 +0800

    完成新版的廣論科判

---
+++ tools/python/listD4Tree.py
@@ -0,0 +1,7 @@
+import xml.etree.ElementTree as ET
+root = ET.parse('TreeNodes.xml').getroot()
+
+
+for idx,child in enumerate(root):
+    print('{{"id":"{}", "parent" : "obj_2", "text" : "{}","a_attr":{{"href":"https://larms.azurewebsites.net/larmbook.html?num={}"}}}},'.format(idx+1,child.attrib["Text"],child.attrib["Page"]))
+
