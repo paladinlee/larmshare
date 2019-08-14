acde1e2335e301d272a53e7679cd896cefc45f44
paladinlee <paladinlee@gmail.com>
2019-08-14 17:58:38 +0800

    全廣更新到 144

--- Scripts/fullLarmData.js
+++ Scripts/fullLarmData.js
@@ -1,4 +1,5 @@
 var arrayDailyLamID = [
+'144',        
 '143',        
 '142',        
 '141',        
@@ -143,6 +144,7 @@ var arrayDailyLamID = [
 '2',
 '1'];
 var arrayDailyLamFile = [
+'https://d3v2d3f6zmz0pr.cloudfront.net/mp3/lr2/%E7%A6%8F%E6%99%BA%E5%85%A8%E7%90%83%E5%BB%A3%E8%AB%96%E7%AC%AC%E4%BA%8C%E8%BC%AA_0144.mp3',        
 'https://d3v2d3f6zmz0pr.cloudfront.net/mp3/lr2/%E7%A6%8F%E6%99%BA%E5%85%A8%E7%90%83%E5%BB%A3%E8%AB%96%E7%AC%AC%E4%BA%8C%E8%BC%AA_0143.mp3',        
 'https://d3v2d3f6zmz0pr.cloudfront.net/mp3/lr2/%E7%A6%8F%E6%99%BA%E5%85%A8%E7%90%83%E5%BB%A3%E8%AB%96%E7%AC%AC%E4%BA%8C%E8%BC%AA_0142.mp3',        
 'https://d3v2d3f6zmz0pr.cloudfront.net/mp3/lr2/%E7%A6%8F%E6%99%BA%E5%85%A8%E7%90%83%E5%BB%A3%E8%AB%96%E7%AC%AC%E4%BA%8C%E8%BC%AA_0141.mp3',        
@@ -289,6 +291,7 @@ var arrayDailyLamFile = [
 ];
 
 var arrayDailyLamUrl = [
+'https://www.blisswisdom.org/teachings/dailylamrim2/3060-130144',        
 'https://www.blisswisdom.org/teachings/dailylamrim2/3059-130143',        
 'https://www.blisswisdom.org/teachings/dailylamrim2/3058-130142',        
 'https://www.blisswisdom.org/teachings/dailylamrim2/3057-130141',        
